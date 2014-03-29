cb5d5be5ead56641cd2e6ca94b97991246cb3d2f
jlblatt <jlblatt+github@gmail.com>
2014-03-28 23:13:34 -0400

    Token Movement

--- js/lib.js
+++ js/lib.js
@@ -15,17 +15,17 @@ function resizeStage(x, y)
 
 function createGrid(depth)
 {
-  var layer = new Kinetic.Layer();
+  _GRIDLAYER = new Kinetic.Layer();
   var hexes = [];
 
   //calculate diamter
-  var diameter = _HEIGHT / (depth * 2);
+  _DIAMETER = _HEIGHT / (depth * 2);
 
   //setup our first cell
   var originHex = new Kinetic.Circle({
     x: _WIDTH/2,
     y: _HEIGHT/2,
-    radius: diameter / 2,
+    radius: _DIAMETER / 2,
     fillRed: 200,
     fillGreen: 200,
     fillBlue: 255,
@@ -33,14 +33,19 @@ function createGrid(depth)
     draggable: true
   });
 
-  layer.add(originHex);
+  originHex.origFillRed = 200;
+  originHex.origFillGreen = 200;
+  originHex.origFillBlue = 255;
+  originHex.origFillAlpha = 1;
+
+  _GRIDLAYER.add(originHex);
   hexes.push(originHex);
 
   //each level of the grid
   for(var level = 1; level < depth; level++)
   {
     var alpha = 1- ((1 / (depth * 1.2)) * level);
-    var currCellCoords = [0, -level * diameter];
+    var currCellCoords = [0, -level * _DIAMETER];
 
     //each cell in this level
     for(hex = 0; hex < level * 6; hex++)
@@ -49,22 +54,89 @@ function createGrid(depth)
       var thisHex = new Kinetic.Circle({
         x: currCellCoords[0] + (_WIDTH/2),
         y: currCellCoords[1] + (_HEIGHT/2),
-        radius: diameter / 2,
+        radius: _DIAMETER / 2,
         fillRed: 200,
         fillGreen: 200,
         fillBlue: 255,
         fillAlpha: alpha
       });
 
-      layer.add(thisHex);
+      originHex.origFillRed = 200;
+      originHex.origFillGreen = 200;
+      originHex.origFillBlue = 255;
+      originHex.origFillAlpha = alpha;
+
+      _GRIDLAYER.add(thisHex);
       hexes.push(thisHex);
 
       //calculate coordinates of next cell (this is the fun part)
-      currCellCoords[0] += diameter * Math.cos((Math.floor(hex / level) * Math.PI/3) + Math.PI/6);
-      currCellCoords[1] += diameter * Math.sin((Math.floor(hex / level) * Math.PI/3) + Math.PI/6);
+      currCellCoords[0] += _DIAMETER * Math.cos((Math.floor(hex / level) * Math.PI/3) + Math.PI/6);
+      currCellCoords[1] += _DIAMETER * Math.sin((Math.floor(hex / level) * Math.PI/3) + Math.PI/6);
     }
   }
 
-  _STAGE.add(layer);
+  //attach events
+  for(var i = 0; i < hexes.length; i++)
+  {
+    hexes[i].on('mousedown touchdown', function() {
+      _TOKEN.x(this.x());
+      _TOKEN.y(this.y());
+      _TOKENLAYER.draw();
+    });
+  }
+
+  _STAGE.add(_GRIDLAYER);
   return hexes;
+}
+
+
+
+function createToken()
+{
+  _TOKENLAYER = new Kinetic.Layer();
+
+  var token = new Kinetic.Circle({
+    x: _WIDTH/2,
+    y: _HEIGHT/2,
+    radius: _DIAMETER / 2,
+    fillRed: 255,
+    fillGreen: 147,
+    fillBlue: 15,
+    fillAlpha: 1,
+    stroke: "#000",
+    strokeWidth: _DIAMETER/10,
+    draggable: true
+  });
+
+  token.on("dragend", function() {
+    var pos = _STAGE.getPointerPosition();
+    dropNearest(this, pos.x, pos.y);
+  });
+
+  _TOKENLAYER.add(token);
+  _STAGE.add(_TOKENLAYER);
+
+  return token;
+}
+
+
+
+function dropNearest(node, x, y)
+{
+  var closestIndex = 0;
+  var closestDist = Number.MAX_VALUE;
+  for(var i = 0; i < _GRID.length; i++)
+  {
+    var dist = Math.sqrt(Math.pow(Math.abs(x - _GRID[i].x()), 2) + Math.pow(Math.abs(y - _GRID[i].y()), 2));
+
+    if(dist < closestDist)
+    {
+      closestIndex = i;
+      closestDist = dist;
+    }
+  }
+
+  node.x(_GRID[closestIndex].x());
+  node.y(_GRID[closestIndex].y());
+  _TOKENLAYER.draw();
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 _WIDTH = 600;
 _HEIGHT = 600;
-_GRIDDEPTH = location.search != "" ? location.search.replace('?', '') : 3;
+_DIAMETER = 0;
+_GRIDDEPTH = location.search != "" ? parseInt(location.search.replace('?', '')) : 3;
 
 _STAGE = new Kinetic.Stage({
   container: 'stage',
@@ -11,3 +12,38 @@ _STAGE = new Kinetic.Stage({
 resizeStage(_WIDTH, _HEIGHT);
 
 _GRID = createGrid(_GRIDDEPTH);
+
+_TOKEN = createToken();
+
+$(window).keyup(function(e)
+{
+  var code = e.keyCode || e.which;
+  e.preventDefault();
+
+  //q or 7
+  if(code == 81 || code == 36)
+    dropNearest(_TOKEN, _TOKEN.x() - _DIAMETER * Math.cos(Math.PI/6), _TOKEN.y() - _DIAMETER * Math.sin(Math.PI/6));
+  
+  //w or 8
+  else if(code == 87 || code == 38)
+    dropNearest(_TOKEN, _TOKEN.x(), _TOKEN.y() - _DIAMETER);
+  
+  //e or 9
+  else if(code == 69 || code == 33)
+    dropNearest(_TOKEN, _TOKEN.x() + _DIAMETER * Math.cos(Math.PI/6), _TOKEN.y() - _DIAMETER * Math.sin(Math.PI/6));
+  
+  //a or 4
+  else if(code == 65 || code == 37)
+    dropNearest(_TOKEN, _TOKEN.x() - _DIAMETER * Math.cos(Math.PI/6), _TOKEN.y() + _DIAMETER * Math.sin(Math.PI/6));
+  
+  
+  //s or 5
+  else if(code == 83 || code == 12)
+    dropNearest(_TOKEN, _TOKEN.x(), _TOKEN.y() + _DIAMETER);
+  
+  //d or 6
+  else if(code == 68 || code == 39)
+    dropNearest(_TOKEN, _TOKEN.x() + _DIAMETER * Math.cos(Math.PI/6), _TOKEN.y() + _DIAMETER * Math.sin(Math.PI/6));
+
+  _TOKENLAYER.draw();
+});
